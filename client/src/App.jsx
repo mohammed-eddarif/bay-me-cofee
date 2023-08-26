eab02447dcd0245b9046b842715decf3d17802ce
mohammed-eddarif <mohammed.eddari06@gmail.com>
2023-08-26 12:52:22 +0100

    Added Tailwind CSS and updated styles in the app.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,75 @@
-import {ethers} from 'ethers';
-import './App.css';
-import { useEffect,useState } from 'react';
-import abi from './contractJson/contracts/chai.sol/chai.json';
-import Buy from './components/Buy';
+import { ethers } from "ethers";
+import "./App.css";
+import { useEffect, useState } from "react";
+import abi from "./contractJson/contracts/chai.sol/chai.json";
+import Buy from "./components/Buy";
 import chai from "./chai.png";
-import Memos from './components/Memos';
+import Memos from "./components/Memos";
 
 function App() {
-
-  const [state,setState] = useState({
-    provider:null,
-    signer:null,
-    contract:null
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
   });
 
-  const [account,setAccount] = useState('Not connected');
+  const [account, setAccount] = useState("Not connected");
 
-  useEffect(()=>{
-    const template =async ()=>{
+  useEffect(() => {
+    const template = async () => {
       const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
       const contractABI = abi.abi;
-     try{
-      const {ethereum} = window;
-      const accnt = await ethereum.request({
-        method:"eth_requestAccounts"
-      }); 
-      
-      window.ethereum.on('accountsChanged',()=>{
-        window.location.reload();
-      });
+      try {
+        const { ethereum } = window;
+        const accnt = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
 
-      setAccount(accnt);
-      
-      const web3Provider = new ethers.BrowserProvider(ethereum);
-      const web3signer = await web3Provider.getSigner();
-      const contract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        web3signer
-      );
+        window.ethereum.on("accountsChanged", () => {
+          window.location.reload();
+        });
 
-     
-      setState({web3Provider,web3signer,contract})
-     }catch (error) {
+        setAccount(accnt);
 
-      console.log(error.message)
-  
-    }
-  }
+        const web3Provider = new ethers.BrowserProvider(ethereum);
+        const web3signer = await web3Provider.getSigner();
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          web3signer
+        );
+
+        setState({ web3Provider, web3signer, contract });
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
     template();
-  },[])
+  }, []);
 
   return (
-    <div>
-      <img src={chai} className="img-fluid" alt=".." width="100%" />
-      <p style={{ marginTop: "10px", marginLeft: "5px" }}>
-        <small>Connected Account - {account}</small>
+    <div className="flex flex-col items-center">
+      <img
+        src={chai}
+        className="shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4 w-full md:w-2/3 lg:w-1/2 xl:w-5/6"
+        alt=".."
+      />
+
+      <p className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4 w-full ">
+        Connected Account -<small> {account}</small>
       </p>
+
+      <blockquote class="text-xl italic font-semibold text-center text-gray-900 dark:text-white">
+        <p className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4 w-full ">
+          "Users have the option to send transactions by adding funds to the
+          platform. Each contribution, regardless of its size, accumulates to
+          form a financial pool dedicated to a specific charitable organization.
+          At the end of a predefined period, the entirety of the accumulated
+          amount is automatically transferred to the charitable organization,
+          thus eliminating traditional barriers and complex fundraising
+          processes."
+        </p>
+      </blockquote>
 
       <Buy state={state} />
       <Memos state={state} />
@@ -64,4 +77,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- client/src/components/Buy.jsx
+++ client/src/components/Buy.jsx
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { useForm } from "react-hook-form";
+import "./Buy.css";
 
 const Buy = ({ state }) => {
   const {
@@ -24,35 +25,49 @@ const Buy = ({ state }) => {
   //    },[]);
 
   return (
-    <div>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="htmlForm-column">
-          <label htmlFor="name">Name:</label>
-          <input {...register("name", { required: true })} />
-          <br />
-          {errors.name && <span>This field is required</span>}
-          <br />
-        </div>
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4 w-full md:w-2/3 lg:w-1/2 xl:w-1/3"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex w-full flex-col ">
+        <label htmlFor="name" className="text-base-semibold text-light-2">
+          Name:
+        </label>
+        <input {...register("name", { required: true })} />
+        <br />
+        {errors.name && <span>This field is required</span>}
+        <br />
+      </div>
 
-        <div className="form-column">
-          <label htmlFor="message">Message:</label>
-          <input {...register("message", { required: true })} />
-          <br />
-          {errors.message && <span>This field is required</span>}
-          <br />
-        </div>
+      <div className="flex w-full flex-col ">
+        <label htmlFor="amount" className="text-base-semibold text-light-2">
+          Amount:
+        </label>
+        <input {...register("amount", { required: true })} />
+        <br />
+        {errors.amount && <span>This field is required</span>}
+        <br />
+      </div>
 
-        <div className="form-column">
-          <label htmlFor="amount">Amount:</label>
-          <input {...register("amount", { required: true })} />
-          <br />
-          {errors.amount && <span>This field is required</span>}
-          <br />
-        </div>
+      <div className="flex w-full flex-col ">
+        <label htmlFor="message" className="text-base-semibold text-light-2">
+          Message:
+        </label>
+        <input {...register("message", { required: true })} />
+        <br />
+        {errors.message && <span>This field is required</span>}
+        <br />
+      </div>
 
-        <input type="submit" value="Pay" />
-      </form>
-    </div>
+      <div className="flex justify-center">
+        <input
+          type="submit"
+          value="Pay"
+          // not applied !!!
+          className="mt-4 w-full md:w-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline cursor-pointer"
+        />
+      </div>
+    </form>
   );
 };
 export default Buy;
